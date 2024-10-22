2a6787faa1bdc7024d9bd9c914a26f10d052814c
Marina982 <159331002+Marina982@users.noreply.github.com>
2024-10-22 19:33:39 -0300

    Arrumar a lista para exibir a mesma

--- src/components/ListarProdutos.jsx
+++ src/components/ListarProdutos.jsx
@@ -1,16 +1,13 @@
-export default function ListarProdutos({listaProdutos}){
-return(
-
-    <ul>
-{
-listaProdutos.map(produto => (
-    <li key={produto.id}>
-<p>{produto.title}</p>
-<p>{produto.price}</p>
-    </li>
-))
-}
-
-</ul>
-    )
+export default function ListarProdutos({ listaProdutos }) {
+    return (
+        <ul>
+            {listaProdutos.map(produto => (
+                <li key={produto.id}>
+                    <img src={produto.image} alt={produto.title} />
+                    <p>{produto.title}</p>
+                    <p>Valor: {produto.price}</p>
+                </li>
+            ))}
+        </ul>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,26 @@
 import { useEffect, useState } from "react";
 import ListarProdutos from "../components/ListarProdutos";
 
-
-
 export default function Home() {
-
-    const [listar, setListar] = useState([])
+    const [listar, setListar] = useState([]);
 
     useEffect(() => {
         const lista = async () => {
-        try{
-            const resposta  = await fetch('https://fakestoreapi.com/products')
-            const dados = await resposta.json()
-            setListar(dados)
-            }catch (error) {
-console.log("A API não carregou")
+            try {
+                const resposta = await fetch('https://fakestoreapi.com/products');
+                const dados = await resposta.json();
+                setListar(dados);
+            } catch (error) {
+                console.log("A API não carregou");
             }
-          lista();
-        }
-    },[]);
+        };
+        lista();
+    }, []);
+
+    return (
+        <div>
+            <h1>Lista Produtos</h1>
+            <ListarProdutos listaProdutos={listar} />
+        </div>
+    );
 }
-
-return(
-<>
-
-<ListarProdutos listaProdutos={produto}></ListarProdutos>
-
-</>
-);
