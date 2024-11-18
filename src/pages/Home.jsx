c91bacd41086e00a0258107592c64108ee1da907
Marina982 <159331002+Marina982@users.noreply.github.com>
2024-11-18 14:10:18 -0300

    save

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Footer from "../components/Footer";
 
 export default function Home() {
     const [listar, setListar] = useState([]);
+    const [pesquisa, setPesquisa] = useState('');
 
     useEffect(() => {
         const lista = async () => {
@@ -19,11 +20,44 @@ export default function Home() {
         lista();
     }, []);
 
-    
+    const orderAZ = () => {
+        const listaOrdenada = [...listar].sort((a, b) => a.title.localeCompare(b.title));
+        setListar(listaOrdenada);
+    }
+
+    const orderZA = () => {
+        const listaOrdenada = [...listar].sort((a, b) => b.title.localeCompare(a.title));
+        setListar(listaOrdenada);
+    }
+
+
+const PrecoMaior = () => {
+    const listaOrdenada = [...listar].sort((a, b) => b.price.localeCompare(a < b));
+    setListar(listaOrdenada);
+}
+
+
+
+    const buscarProdutos = (termo) => {
+        setPesquisa(termo);
+        const produtosFiltrados = listar.filter(produto => 
+            produto.title.toLowerCase().includes(termo.toLowerCase())
+        );
+        setListar(produtosFiltrados);
+    }
+
+
 
     return (
         <div>
-             <Header/>
+            <Header/>
+            <input 
+                placeholder="Pesquisar" 
+                onChange={(event) => buscarProdutos(event.target.value)}
+            />
+            <button onClick={() => orderAZ()}>Filtro a AZ</button>
+            <button onClick={() => orderZA()}>Filtro a ZA</button>
+            <button onClick={() => PrecoMaior()}>Filtro Preço</button>
             <ListarProdutos listaProdutos={listar} />
             <Footer/>
         </div>
