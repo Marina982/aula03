57f7666459d32ad0007b2b79cb8b0cc7eeb005e0
Marina982 <159331002+Marina982@users.noreply.github.com>
2024-11-26 19:31:16 -0300

    save

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,19 @@ import { useEffect, useState } from "react";
 import ListarProdutos from "../components/ListarProdutos";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import styles from '../styles/Header.module.css'
+import styles from '../styles/Header.module.css';
 
 export default function Home() {
+    const [produtos, setProdutos] = useState([]); 
     const [listar, setListar] = useState([]);
-    const [pesquisa, setPesquisa] = useState([]);
+    const [pesquisa, setPesquisa] = useState('');
 
     useEffect(() => {
         const lista = async () => {
             try {
                 const resposta = await fetch('https://fakestoreapi.com/products');
                 const dados = await resposta.json();
+                setProdutos(dados); 
                 setListar(dados);
             } catch (error) {
                 console.log("A API não carregou");
@@ -31,28 +33,23 @@ export default function Home() {
         setListar(listaOrdenada);
     }
 
-
     const PrecoMaior = () => {
         const listaOrdenada = [...listar].sort((a, b) => b.price - a.price);
         setListar(listaOrdenada);
     }
 
-
     const PrecoMenor = () => {
         const listaOrdenada = [...listar].sort((a, b) => a.price - b.price);
         setListar(listaOrdenada);
     }
-    
 
     const buscarProdutos = (termo) => {
         setPesquisa(termo);
-        const produtosFiltrados = listar.filter(produto => 
+        const produtosFiltrados = produtos.filter(produto => 
             produto.title.toLowerCase().includes(termo.toLowerCase())
         );
         setListar(produtosFiltrados);
     }
-
-
 
     return (
         <div>
@@ -60,13 +57,12 @@ export default function Home() {
             <input 
                 placeholder="Pesquisar" 
                 onChange={(event) => buscarProdutos(event.target.value)}
-            className={styles.BarradeNavegacaos}/>
-            
+                className={styles.BarradeNavegacaos}
+            />
             <button onClick={() => orderAZ()} className={styles.botoes}>Filtro a AZ</button>
             <button onClick={() => orderZA()} className={styles.botoes}>Filtro a ZA</button>
             <button onClick={() => PrecoMaior()} className={styles.botoes}>Filtro Preço Maior</button>
             <button onClick={() => PrecoMenor()} className={styles.botoes}>Filtro Preço Menor</button>
-            
             <ListarProdutos listaProdutos={listar} />
             <Footer/>
         </div>
